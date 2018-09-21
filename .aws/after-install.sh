4fe322bf04ec6d85730a2b1239a6608247e3fef6
Danny van der Sluijs <dvandersluijs@monitorlinq.com>
2018-09-21 20:11:21 +0200

    Change from native html/webpack to Angular6 solution

--- .aws/after-install.sh
+++ .aws/after-install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#######################################################################
-# Application preparation
-#######################################################################
 
 (
     cd /var/www/html;
 
-    # Compile CSS and JS
     npm install;
     npm run build;
 )
--- src/main.js
+++
@@ -1,7 +0,0 @@
-//var $ = require('jquery/src/core');
-window.jQuery = window.$ =  require('jquery/dist/jquery.min');
-require('jquery/src/core/init');
-require('jquery/src/manipulation');
-require('bootstrap/dist/js/bootstrap');
-require('bootstrap/dist/css/bootstrap.css');
-require('./cover.css');
